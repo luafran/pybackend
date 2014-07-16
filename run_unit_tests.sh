4441b8130e34983a7696eb382744fa9195795fd5
Luciano A <lucio@homero.lucio01.net>
2014-07-16 20:56:50 -0300

    Use nosetests to run tests

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -4,10 +4,14 @@ scriptname=`basename $0`
 echo "--------> $scriptname"
 
 src_dir=./src
+test_dir=./src/test
 result_dir=unit_test-results
-report_dir=coverage-report
+report_dir=$PWD/coverage-report
 rm -rf $result_dir
 rm -rf $report_dir
 
-coverage run --source $src_dir ./runtest.py discover
-coverage html --directory $report_dir --omit=*/test*,test*,*__init__*
+#coverage run --source $src_dir ./runtest.py discover
+#coverage html --directory $report_dir --omit=*/test*,test*,*__init__*
+
+# TODO: show test time in stdout
+nosetests -v --nocapture --with-timer --with-coverage --cover-html --cover-html-dir=$report_dir --cover-inclusive --cover-erase --cover-package=$src_dir --with-xunit --where $test_dir
